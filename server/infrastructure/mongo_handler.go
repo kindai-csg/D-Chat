638e9e4b01b5d753325ecfccba01daf43e4a5454
K.Tsukimoto <t.29.wuq@gmail.com>
2020-08-07 20:20:47 +0900

    :+1: InsertOneの追加

--- server/infrastructure/mongo_handler.go
+++ server/infrastructure/mongo_handler.go
@@ -3,12 +3,16 @@ package infrastructure
 import (
 	"context"
 
+	"github.com/kindai-csg/D-Chat/interfaces/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoHandler struct {
-	client *mongo.Client
+	client   *mongo.Client
+	database *mongo.Database
 }
 
 func NewMongoHandler() (*MongoHandler, error) {
@@ -17,7 +21,28 @@ func NewMongoHandler() (*MongoHandler, error) {
 		return nil, err
 	}
 	mongoHandler := MongoHandler{
-		client: client,
+		client:   client,
+		database: client.Database("dchat"),
 	}
 	return &mongoHandler, nil
+}
+
+func (handler *MongoHandler) castKvToBson(doc []database.KV) bson.D {
+	result := bson.D{}
+	for _, kv := range doc {
+		e := primitive.E{
+			Key:   kv.Key,
+			Value: kv.Value,
+		}
+		result = append(result, e)
+	}
+	return result
+}
+
+func (handler *MongoHandler) InsertOne(collectionName string, doc []database.KV) (string, error) {
+	result, err := handler.database.Collection(collectionName).InsertOne(context.Background(), handler.castKvToBson(doc))
+	if err != nil {
+		return nil, err
+	}
+	return string(result.InsertedID.(primitive.ObjectID)), nil
 }
---
+++ server/interfaces/database/kv.go
@@ -0,0 +1,6 @@
+package database
+
+type KV struct {
+	Key   string
+	Value interface{}
+}
