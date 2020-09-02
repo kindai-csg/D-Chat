79c72baea37aae6719f77e96336cdf1b78063d5f
K.Tsukimoto <t.29.wuq@gmail.com>
2020-09-03 01:54:09 +0900

    :+1: add CreateUser

--- server/interfaces/controllers/user_controller.go
+++ server/interfaces/controllers/user_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/kindai-csg/D-Chat/interfaces/database"
+	"github.com/kindai-csg/D-Chat/interfaces/gateway"
 	"github.com/kindai-csg/D-Chat/usecase"
 )
 
@@ -13,4 +14,16 @@ func NewUserController(mongoHandler database.MongoHandler) *UserController {
 	userController := UserController{
 		interactor: *usecase.NewUserInteractor(database.NewUserRepository(mongoHandler)),
 	}
+	return &userController
+}
+
+func (controller *UserController) CreateUser(c Context) {
+	input := gateway.UserInput{}
+	c.Bind(&input)
+	user, err := controller.interactor.Create(gateway.CreateUserFromUserInput(input))
+	if err != nil {
+		c.JSON(500, gateway.StatusMessageOutput{false, "Failed to create account."})
+		return
+	}
+	c.JSON(200, gateway.CreateUserInfoOutputFromUser(user))
 }
