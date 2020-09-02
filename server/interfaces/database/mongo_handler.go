9feadca58a3295684429d8965f5045ca04c555ff
K.Tsukimoto <t.29.wuq@gmail.com>
2020-09-03 01:56:23 +0900

    :+1: fixfix

--- server/interfaces/database/mongo_handler.go
+++ server/interfaces/database/mongo_handler.go
@@ -1,9 +1,10 @@
 package database
 
 type MongoHandler interface {
-	CreateIndex(string, []KV, ...[]KV) error
+	CreateIndex(string, []KV, []KV) error
 	Insert(string, []KV) (string, error)
 	Find(string, []KV) ([][]KV, error)
+	FindOne(string, []KV) ([]KV, error)
 	Update(string, []KV, []KV) error
 	Delete(string, []KV) error
 }
