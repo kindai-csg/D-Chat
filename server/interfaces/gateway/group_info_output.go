7cb3b7a0481866f3193970443953b0444b80d8af
山本　瑛悟 <yamamotoeigo@yamamotoakirasatorunoMacBook-Pro.local>
2020-12-06 23:27:11 +0900

    make_group

--- server/interfaces/gateway/group_info_output.go
+++ server/interfaces/gateway/group_info_output.go
@@ -1,7 +1,17 @@
 package gateway
 
-type CreateGroupInfoOutputFromGroup struct {
+import "github.com/kindai-csg/D-Chat/domain"
+
+type UserInfoOutput struct {
 	Name         string `json:"name"`
 	About        string `json:"about"`
 	Participants string `json:"participants"`
+}
+
+func CreateGroupInfoOutputFromGroup(group domain.Group) GroupInfoOutput {
+	return GroupInfoOutput{
+		group.Name,
+		group.About,
+		group.Participants,
+	}
 }
